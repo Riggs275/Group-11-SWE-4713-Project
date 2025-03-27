c516710a51ec3883d795514be7ef0cc36e6efd45
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-03-27 14:26:59 -0400

    Create AccountController.java

---
+++ src/Components/Controller/AccountController.java
@@ -0,0 +1,46 @@
+package com.accountingAPI.accountingSoftware.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    // Add new account
+    @PostMapping("/addAccount")
+    public ResponseEntity<?> addAccount(@RequestBody Map<String, String> accountData) {
+        return accountService.addAccount(accountData);
+    }
+
+    // Edit account details
+    @PutMapping("/editAccount")
+    public ResponseEntity<?> editAccount(@RequestBody Map<String, String> accountData) {
+        return accountService.editAccount(accountData);
+    }
+
+    // View individual account
+    @GetMapping("/viewAccounts")
+    public ResponseEntity<?> viewAccounts(@RequestBody Map<String, String> accessorData) {
+        return accountService.viewAccount(accessorData);
+    }
+
+    // Set active status of an account
+    @PostMapping("/setActive")
+    public ResponseEntity<?> setActive(@RequestBody Map<String, String> accountData) {
+        return accountService.setActive(accountData);
+    }
+
+    // Retrieve the ledger for a given account
+    @PostMapping("/getLedger")
+    public ResponseEntity<?> getLedger(@RequestBody Map<String, String> ledgerData) {
+        return accountService.getLedger(ledgerData);
+    }
+
+    // Get full chart of accounts
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllAccounts() {
+        return ResponseEntity.getAllAccounts(accountService.getAllAccounts());
+    }
+}
