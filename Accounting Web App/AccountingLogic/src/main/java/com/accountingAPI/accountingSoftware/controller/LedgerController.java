abeb32a88b4081e97e397b9caadea9d84dcdf032
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-14 15:18:22 -0400

    Create LedgerController.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/controller/LedgerController.java
@@ -0,0 +1,31 @@
+package com.accountingAPI.accountingSoftware.controllers;
+
+import com.accountingAPI.accountingSoftware.services.LedgerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/ledger")
+@CrossOrigin(origins = "http://localhost:3000")
+public class LedgerController {
+
+    @Autowired
+    private LedgerService ledgerService;
+
+    @PostMapping("/post")
+    public ResponseEntity<?> postJournalToLedger(@RequestBody Map<String, String> data) {
+        return ledgerService.postJournalToLedger(data);
+    }
+
+    @PostMapping("/get")
+    public ResponseEntity<?> getLedgerByAccount(@RequestBody Map<String, String> data) {
+        return ledgerService.getLedgerByAccount(data);
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> searchLedger(@RequestBody Map<String, String> data) {
+        return ledgerService.searchLedger(data);
+    }
+}
