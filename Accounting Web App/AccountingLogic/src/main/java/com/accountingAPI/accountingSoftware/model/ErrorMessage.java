aaa96d4a07b79106013c5546b11d8445c4c7184b
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-14 13:44:30 -0400

    Create ErrorMessage.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/model/ErrorMessage.java
@@ -0,0 +1,42 @@
+package com.accountingAPI.accountingSoftware.components;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "error_messages")
+public class ErrorMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "error_id")
+    private int errorId;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    public ErrorMessage() {}
+
+    // Getters and Setters
+    public int getErrorId() {
+        return errorId;
+    }
+    public void setErrorId(int id) {
+        id = errorId;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String newMessage) {
+        newMessage = message;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(LocalDateTime timestampSet) {
+        timestampSet = timestamp;
+    }
+}
