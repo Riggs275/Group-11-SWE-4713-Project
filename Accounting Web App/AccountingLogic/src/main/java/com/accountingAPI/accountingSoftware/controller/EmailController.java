6d41f69b516f29f53d6826b0b37877a944b1aca0
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-14 15:19:19 -0400

    Create EmailController.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/controller/EmailController.java
@@ -0,0 +1,20 @@
+package com.accountingAPI.accountingSoftware.controllers;
+
+import com.accountingAPI.accountingSoftware.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+ 
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin(origins = "http://localhost:3000")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        return emailService.sendEmail(to, subject, body);
+    }
+}
