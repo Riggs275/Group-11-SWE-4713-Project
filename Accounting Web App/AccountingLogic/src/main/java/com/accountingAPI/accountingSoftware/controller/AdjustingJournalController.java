58faa76371ac6d084d1b93599caf04b83c81e587
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-14 15:17:47 -0400

    Create AdjustingJournalController.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/controller/AdjustingJournalController.java
@@ -0,0 +1,56 @@
+package com.accountingAPI.accountingSoftware.controllers;
+
+import com.accountingAPI.accountingSoftware.services.AdjustingJournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/adjusting")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AdjustingJournalController {
+
+    @Autowired
+    private AdjustingJournalService adjustingJournalService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createAdjustingEntry(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.createAdjustingEntry(data);
+    }
+
+    @PostMapping("/approve")
+    public ResponseEntity<?> approveAdjustingEntry(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.approveAdjustingEntry(data);
+    }
+
+    @PostMapping("/reject")
+    public ResponseEntity<?> rejectAdjustingEntry(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.rejectAdjustingEntry(data);
+    }
+
+    @PostMapping("/cancel")
+    public ResponseEntity<?> cancelAdjustingEntry(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.cancelAdjustingEntry(data);
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<?> submitAdjustingEntry(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.submitAdjustingEntry(data);
+    }
+
+    @PostMapping("/status")
+    public ResponseEntity<?> getAdjustingEntriesByStatus(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.getAdjustingEntriesByStatus(data);
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> searchAdjustingEntries(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.searchAdjustingEntries(data);
+    }
+
+    @PostMapping("/attach")
+    public ResponseEntity<?> attachSourceDocument(@RequestBody Map<String, String> data) {
+        return adjustingJournalService.attachSourceDocument(data);
+    }
+}
