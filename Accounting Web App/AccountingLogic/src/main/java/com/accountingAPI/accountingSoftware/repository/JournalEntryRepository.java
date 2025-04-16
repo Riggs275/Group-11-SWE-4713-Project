c78a1a4f78214900f4bcda9edf64d50f39bb478a
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-16 15:22:35 -0400

    Create JournalEntryRepository.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package com.accountingAPI.accountingSoftware.repositories;
+
+import com.accountingAPI.accountingSoftware.components.JournalEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+@Repository
+public interface JournalEntryRepository extends JpaRepository<JournalEntry, Integer> {
+    List<JournalEntry> findByStatus(String status);
+}
