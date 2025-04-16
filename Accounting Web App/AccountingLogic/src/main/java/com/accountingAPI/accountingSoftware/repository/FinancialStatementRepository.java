1d904a885928c82aeb883f5c95d726c5fd5ccfec
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-16 15:22:34 -0400

    Create FinancialStatementRepository.java

---
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/FinancialStatementRepository.java
@@ -0,0 +1,12 @@
+package com.accountingAPI.accountingSoftware.repositories;
+
+import com.accountingAPI.accountingSoftware.components.FinancialStatement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface FinancialStatementRepository extends JpaRepository<FinancialStatement, Integer> {
+    List<FinancialStatement> findByFromDateGreaterThanEqualAndToDateLessThanEqual(LocalDate fromDate, LocalDate toDate);
+}
