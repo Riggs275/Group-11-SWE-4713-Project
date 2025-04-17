84ffd434a5d235323af899cd8b09f04f66daf3b4
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-17 13:53:36 -0400

    Update JournalEntryRepository.java

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/JournalEntryRepository.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/JournalEntryRepository.java
@@ -1,4 +1,4 @@
-package com.accountingAPI.accountingSoftware.repositories;
+package com.accountingAPI.accountingSoftware.repository;
 
 import com.accountingAPI.accountingSoftware.components.JournalEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
