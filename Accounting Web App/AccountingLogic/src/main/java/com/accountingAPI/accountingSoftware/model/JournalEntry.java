d2e2ff59d92b5e7bbc989f38caf3cfd0b4046f22
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-23 12:35:21 -0400

    Fixed incorrect setter methods

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/model/JournalEntry.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/model/JournalEntry.java
@@ -42,42 +42,42 @@ public class JournalEntry {
         return entryId;
     }
     public void setEntryId(int id) {
-        id = entryId;
+        entryId = id;
     }
     public LocalDate getDate() {
         return date;
     }
     public void setDate(LocalDate dateSet) {
-        dateSet = date;
+        date = dateSet;
     }
     public String getSubmittedBy() {
         return submittedBy;
     }
     public void setSubmittedBy(String userSubmitted) {
-        userSubmitted = submittedBy;
+        submittedBy = userSubmitted;
     }
     public String getStatus() {
         return status;
     }
     public void setStatus(String statusSet) {
-        statusSet = status;
+        status = statusSet;
     }
     public String getComment() {
         return comment;
     }
     public void setComment(String newComment) {
-        newComment = comment;
+        comment = newComment;
     }
     public List<JournalLine> getLines() {
         return lines;
     }
     public void setLines(List<JournalLine> addLines) {
-        addLines = lines;
+       lines = addLines;
     }
     public List<ErrorMessage> getErrorMessages() {
         return errorMessages;
     }
     public void setErrorMessages(List<ErrorMessage> error) {
-        error = errorMessages;
+        errorMessages = error;
     }
 }
