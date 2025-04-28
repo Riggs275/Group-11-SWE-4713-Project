b0db59bb2d7567b7049ca8e998f01766aa033cc4
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-28 18:48:17 -0400

    Update FinancialStatementRepository.java

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/FinancialStatementRepository.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/FinancialStatementRepository.java
@@ -2,6 +2,8 @@ package com.accountingAPI.accountingSoftware.repository;
 
 import com.accountingAPI.accountingSoftware.model.FinancialStatement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,4 +12,10 @@ import java.util.List;
 @Repository
 public interface FinancialStatementRepository extends JpaRepository<FinancialStatement, Integer> {
     List<FinancialStatement> findByFromDateGreaterThanEqualAndToDateLessThanEqual(LocalDate fromDate, LocalDate toDate);
+
+    @Query("SELECT f.type FROM FinancialStatement f WHERE f.id = :id")
+    String getType(@Param("id") int id);
+
+    @Query("SELECT f.fileData FROM FinancialStatement f WHERE f.id = :id")
+    byte[] getFileData(@Param("id") int id);
 }
