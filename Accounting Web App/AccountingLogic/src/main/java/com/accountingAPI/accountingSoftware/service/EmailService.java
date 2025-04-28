23872bcdca2f685fea08b5cab0ad2be21928381d
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-28 18:21:46 -0400

    Add method sendEmailWithAttachment()

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/service/EmailService.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/service/EmailService.java
@@ -1,9 +1,15 @@
 package com.accountingAPI.accountingSoftware.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -11,7 +17,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String toEmail, String subject, String content) {
+    public ResponseEntity<?> sendEmail(String toEmail, String subject, String content) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("no-reply@mailslurp.biz"); // ✅ Must match MailSlurp domain
         message.setTo(toEmail);
@@ -20,5 +26,29 @@ public class EmailService {
 
         mailSender.send(message);
         System.out.println("✅ Email sent successfully to " + toEmail);
+        return null;
+    }
+
+    public void sendEmailWithAttachment(String recipient, String subject, String text, byte[] fileData, String fileName) throws Exception {
+
+        MimeMessage mimeMsg = null;
+        MimeMessageHelper msgHelper = null;
+
+        try {
+            mimeMsg = mailSender.createMimeMessage();
+            msgHelper = new MimeMessageHelper(mimeMsg, true); // true => multipart message
+
+            msgHelper.setTo(recipient);
+            msgHelper.setSubject(subject);
+            msgHelper.setText(text);
+
+            ByteArrayResource attachResource = new ByteArrayResource(fileData);
+            msgHelper.addAttachment(fileName, attachResource);
+
+            mailSender.send(mimeMsg);
+        }
+        catch (MessagingException e) {
+            throw new Exception("Failed to send email", e);
+        }
     }
 }
