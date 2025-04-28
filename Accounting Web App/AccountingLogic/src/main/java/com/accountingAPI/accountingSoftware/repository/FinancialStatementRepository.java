905d81b831094dc2d7b5427dedf5e0b92c7138f9
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-28 17:15:07 -0400

    Fixed imports

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/FinancialStatementRepository.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/FinancialStatementRepository.java
@@ -1,7 +1,8 @@
 package com.accountingAPI.accountingSoftware.repository;
 
-import com.accountingAPI.accountingSoftware.components.FinancialStatement;
+import com.accountingAPI.accountingSoftware.model.FinancialStatement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
