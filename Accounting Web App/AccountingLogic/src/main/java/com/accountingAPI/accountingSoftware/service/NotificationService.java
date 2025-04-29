f898b3d294ce56a0cb81b43853c6d0679da0893c
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-29 14:20:30 -0400

    Update NotificationService.java

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/service/NotificationService.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/service/NotificationService.java
@@ -3,10 +3,11 @@ package com.accountingAPI.accountingSoftware.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import com.accountingAPI.accountingSoftware.model.Users;
 
 import com.accountingAPI.accountingSoftware.repository.UserRepository;
 
@@ -16,11 +17,14 @@ public class NotificationService {
     @Autowired
     private UserRepository userRepo;
 
-    public void notifyManager(String message) {
-        List<User> managerUsers = userRepository.findByRole("Manager");
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void notifyManager(String message, String notificationMessage) {
+        List<Users> managerUsers = userRepo.findByRole("Manager");
         
         for (int i = 0; i < managerUsers.size(); i++) {
-            User managerUser = managerUsers.get(i);
+            Users managerUser = managerUsers.get(i);
             SimpleMailMessage notif = new SimpleMailMessage();
             notif.setTo(managerUser.getEmail());
             notif.setSubject("Radiant Flow Accounting");
