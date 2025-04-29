49d6ff1245762f76bb797e710e144e9591bc4b8b
Acervan5 <125931096+Acervan5@users.noreply.github.com>
2025-04-29 17:54:44 -0400

    Update JournalEntryRepository.java

--- Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/JournalEntryRepository.java
+++ Accounting Web App/AccountingLogic/src/main/java/com/accountingAPI/accountingSoftware/repository/JournalEntryRepository.java
@@ -2,7 +2,10 @@ package com.accountingAPI.accountingSoftware.repository;
 
 import com.accountingAPI.accountingSoftware.model.JournalEntry;
 import com.accountingAPI.accountingSoftware.model.JournalLine;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,14 @@ import java.util.List;
 @Repository
 public interface JournalEntryRepository extends JpaRepository<JournalEntry, Integer> {
     List<JournalEntry> findByStatus(String status);
+
+    int countByStatus(String string);
+
+    @Query("""
+      SELECT je
+        FROM JournalEntry je
+        JOIN je.lines jl
+      WHERE jl.accountName = ?1
+    """)
+    List<JournalEntry> findByLineAccountName(String accountName);
 }
